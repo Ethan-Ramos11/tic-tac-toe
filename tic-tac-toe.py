888c267b9347c5d0593562cec4266cff10e36468
Ethan Ramos <ethanramos@Ethans-MacBook-Pro-7.local>
2024-01-31 17:47:42 -0500

    Added all functions and coded functionality to build and print the board

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,32 @@
 import questionary
 
+def createBoard():
+    return [['_','_','_'],['_','_','_'],['_','_','_']]
+
+def printBoard(board):
+    boardStr = ""
+    for row in board:
+        boardStr += "|"
+        for elem in row:
+            boardStr += elem + "|"
+        boardStr += "\n"
+    return boardStr
+
+print(printBoard(createBoard()))
+def validMove(board, move):
+    pass
+
+def checkWin(board):
+    pass
+
+def startGameEasy():
+    pass
+
+def startGameMedium():
+    pass
+
+def startGameHard():
+    pass
 
 def main():
     print("Welcome to tic-tac-toe!")
