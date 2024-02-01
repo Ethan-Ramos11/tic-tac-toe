b985ab5751692c1076e12332517bdbc821b17302
Ethan Ramos <ethanramos@ethans-mbp-7.dhcp.hofstra.edu>
2024-02-01 13:35:21 -0500

    Added user query to get their move

--- player.py
+++ player.py
@@ -2,6 +2,27 @@ import questionary
 
 
 def validMove(board, move):
-    if board[move[0]][move[1]] != "_":
+    if board[move[0] - 1][move[1] - 1] != "_":
         return False
     return True
+
+
+def validateMove(move):
+    if not move.isnumeric():
+        return "Must be a number"
+    elif int(move) > 3 or int(move) < 1:
+        return "Must be from 1-3"
+    return True
+
+
+def getMove():
+    row = questionary.text(
+        "Enter the row from 1-3",
+        validate=validateMove
+    ).ask()
+
+    col = questionary.text(
+        "Enter the col from 1-3",
+        validate=validateMove
+    ).ask()
+    return (int(row), int(col))
